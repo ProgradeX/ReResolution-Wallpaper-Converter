5ec2ffff06c3b10c840e1ca8efaa4a70664203c2
ProgradeX <blockybro02@gmail.com>
2023-06-10 16:56:38 +0500

    trying to put panels in data structure

--- main.py
+++ main.py
@@ -1,5 +1,65 @@
 from PIL import Image
 
+class monitor():
+    def __init__(self, width_cm, height_cm, width_px, height_px):
+        self.above = None
+        self.below = None
+        self.left = None
+        self.right = None
+
+        self.width = width_cm
+        self.height = height_cm
+        self.res_x = width_px
+        self.res_y = height_px
+        return
+
+
+class monitor_config():
+    def __init__(self) -> None:
+        self.anchor = None
+        # self.direction = {'up': 0, 'down': 1, 'left': 2, 'right': 3}
+        pass
+
+        # wow this is some really bad code but dw ill totally not forget and fix this
+    def show_config(self):
+        print('ANCR->', self.anchor.res_x, self.anchor.res_y)
+        if self.anchor.above:
+            print('ABOV->', self.anchor.above.res_x, self.anchor.above.res_y)
+        if self.anchor.below:
+            print('BELW->', self.anchor.below.res_x, self.anchor.below.res_y)
+        if self.anchor.left:
+            print('LEFT->', self.anchor.left.res_x, self.anchor.left.res_y)
+        if self.anchor.right:
+            print('RIGT->', self.anchor.right.res_x, self.anchor.right.res_y)
+        
+
+
+    def add_panel(self, panel, direction = None):
+        if self.anchor is None:
+            print('is anchor')
+            # set monitor to anchor
+            self.anchor = panel
+
+        elif direction is not None:
+            print('has a direction')
+            # set monitor next to anchor
+            if direction == 'above':
+                self.anchor.above = panel
+            elif direction == 'below':
+                self.anchor.below = panel
+            elif direction == 'left':
+                self.anchor.left = panel
+            elif direction == 'right':
+                self.anchor.right = panel
+            else:
+                print('wat')
+
+        else:
+            print('bruh')
+
+        return
+
+
 def calculate_scale():
     num_monitors = 2 # 2 by default
     sizes = []
@@ -45,33 +105,47 @@ def calculate_scale():
     return resolutions, scaled_resolutions
     
 
-input_image = Image.open("input.png")
-size = input_image.size
+def main():
+    input_image = Image.open("input.png")
+    size = input_image.size
 
-res, scaled_res = calculate_scale()
+    res, scaled_res = calculate_scale()
 
-#Crop the input image to 3425x1200 pixels
-if (size[0] < 3425) and (size[1] < 1200):
-    exit
+    #Crop the input image to 3425x1200 pixels
+    if (size[0] < 3425) and (size[1] < 1200):
+        exit
 
-else:
-    xStart = (size[0] - 3425) / 2
-    yStart = (size[1] - 1200) / 2
-    
-    xOffset , yOffset = 0, -100
+    else:
+        xStart = (size[0] - 3425) / 2
+        yStart = (size[1] - 1200) / 2
+        
+        xOffset , yOffset = 0, -100
 
-    cropped_image = input_image.crop((xStart + xOffset, yStart + yOffset, xStart + 3425 + xOffset, yStart + 1200 + yOffset))
+        cropped_image = input_image.crop((xStart + xOffset, yStart + yOffset, xStart + 3425 + xOffset, yStart + 1200 + yOffset))
 
 
-# Select the area (2035, 0) to (3325, 1111) and scale it to 1283x1024 pixels
-selected_area = cropped_image.crop((2035, 0, 3425, 1111))
+    # Select the area (2035, 0) to (3325, 1111) and scale it to 1283x1024 pixels
+    selected_area = cropped_image.crop((2035, 0, 3425, 1111))
 
-monitorOne = cropped_image.crop((0, 0, 1920, 1200))
-monitorTwo = selected_area.resize((1280, 1024))
+    monitorOne = cropped_image.crop((0, 0, 1920, 1200))
+    monitorTwo = selected_area.resize((1280, 1024))
 
-# Move the scaled image after 1920 pixels
-new_image = Image.new('RGB', (3200, 1200))
-new_image.paste(monitorOne, (0, 0))
-new_image.paste(monitorTwo, (1920, 0))
+    # Move the scaled image after 1920 pixels
+    new_image = Image.new('RGB', (3200, 1200))
+    new_image.paste(monitorOne, (0, 0))
+    new_image.paste(monitorTwo, (1920, 0))
 
-new_image.save("output.png")
+    new_image.save("output.png")
+    return
+
+
+# CODE STARTS HERE
+
+m1 = monitor(51, 30, 1920, 1200)
+m2 = monitor(51, 30, 1920, 1650)
+config = monitor_config()
+
+config.add_panel(m1)
+config.add_panel(m2, 'above')
+
+config.show_config()
